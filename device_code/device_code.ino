59b828b1648a0b6ace3578d453563491497ad360
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2024-06-23 22:38:05 +0000

    Add LCD and DHT11

--- device_code/device_code.ino
+++ device_code/device_code.ino
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <DHT.h>
 #include <Firebase_ESP_Client.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <ArduinoJson.h>
 #include <Preferences.h>
+#include <LiquidCrystal_I2C.h>
 // Provide the token generation process info.
 #include "addons/TokenHelper.h"
 // Provide the RTDB payload printing info
@@ -14,8 +16,11 @@
 
 #define MOIS_PIN 34 // ESP32 pin GPIO34 (ADC0) that connects to AOUT pin of moisture sensor
 #define TEMP_PIN  17
+#define DHT11_PIN 14
 #define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for microseconds to seconds */
 #define TIME_TO_SLEEP  5            /* Time ESP32 will go to sleep (in seconds) */
+#define LCD_COL 16
+#define LCD_ROW 2
 
 RTC_DATA_ATTR String farmerId;
 
@@ -27,20 +32,29 @@ FirebaseConfig config;
 bool signupOK = false;
 
 Preferences preferences;
+DHT dht11(DHT11_PIN, DHT11);
 
 float soilTemp;
 int soilMois;
 float pH;
 String npk;
+float humi;
+float tempC;
+unsigned long lastUpdate = 0;
+int displayMode = 0;
 
 
 OneWire oneWire(TEMP_PIN);
 DallasTemperature DS18B20(&oneWire);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup(){
   Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   DS18B20.begin();
+  dht11.begin();
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
 
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED){
@@ -51,6 +65,8 @@ void setup(){
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
+
+  pinMode(BUILTIN_LED, OUTPUT);
   
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
@@ -86,20 +102,25 @@ void setup(){
 
 void loop() {
 
+  lcdScreenControl();
+
   // Put your main code here, to run repeatedly
   if (Firebase.ready()){
     DS18B20.requestTemperatures();       // Send the command to get temperatures
+    
     soilTemp = DS18B20.getTempCByIndex(0);  // Read temperature in °C
     soilMois = analogRead(MOIS_PIN); // Moisture sensor
-    
     pH = 7; // dummy Soil pH
     npk = "7-14-7"; // dummy Soil NPK
+    humi  = dht11.readHumidity();
+    tempC = dht11.readTemperature();
 
     String basePath = "iot/" + farmerId;
 
     // Write NPK to the database path device/npk
     if (Firebase.RTDB.setString(&fbdo, basePath + "/npk", npk)){
-      Serial.println("NPK SENT");
+      Serial.print("NPK SENT ");
+      Serial.println(npk);
     } else {
       Serial.println("NPK FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
@@ -107,7 +128,8 @@ void loop() {
 
     // Write temperature to the database path device/temp
     if (Firebase.RTDB.setFloat(&fbdo, basePath + "/temp", soilTemp)){
-      Serial.println("TEMP SENT");
+      Serial.print("TEMP SENT ");
+      Serial.println(soilTemp);
     } else {
       Serial.println("TEMP FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
@@ -115,7 +137,8 @@ void loop() {
 
     // Write soil moisture to the database path device/mois
     if (Firebase.RTDB.setInt(&fbdo, basePath + "/mois", soilMois)){
-      Serial.println("MOIS SENT");
+      Serial.print("MOIS SENT ");
+      Serial.println(soilMois);
     } else {
       Serial.println("MOIS FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
@@ -123,9 +146,28 @@ void loop() {
 
     // Write soil pH to the database path device/ph
     if (Firebase.RTDB.setInt(&fbdo, basePath + "/ph", pH)){
-      Serial.println("PH SENT");
+      Serial.print("PH SENT ");
+      Serial.println(pH);
     } else {
       Serial.println("PH FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    // Write air humidity
+    if (Firebase.RTDB.setFloat(&fbdo, basePath + "/humidity", humi)){
+      Serial.print("HUMI SENT ");
+      Serial.println(humi);
+    } else {
+      Serial.println("HUMI FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    // Write air temp
+    if (Firebase.RTDB.setFloat(&fbdo, basePath + "/air-temp", tempC)){
+      Serial.print("AIR TEMP SENT ");
+      Serial.println(tempC);
+    } else {
+      Serial.println("AIR TEMP FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
     }
 
@@ -154,4 +196,44 @@ String getFarmerId(const char* serialNumber) {
     Serial.printf("Failed to get document: %s\n", fbdo.errorReason().c_str());
   }
   return "";
+}
+
+//update readings values
+void lcdScreenControl() {
+  humi  = dht11.readHumidity();
+  tempC = dht11.readTemperature();
+
+  // Calculate the time difference since the last update
+  unsigned long timeDiff = millis() - lastUpdate;
+
+  // Rotate the display every 6 seconds
+  if (timeDiff > 3000) {
+    displayMode = (displayMode + 1) % 2; // Rotate between 0, 1, and 2
+    lastUpdate = millis(); // Update the last update time
+    lcd.clear(); // Clear the display for new content
+  }
+
+  // Display based on the current mode
+  switch (displayMode) {
+    case 0: // Display temperature and LDR
+      lcd.setCursor(0, 0);
+      lcd.print("A-Temp:");
+      lcd.setCursor(8, 0); 
+      lcd.print(tempC);
+      lcd.setCursor(0, 1);
+      lcd.print("S-Temp:");
+      lcd.setCursor(8, 1);
+      lcd.print(soilTemp);
+      break;
+    case 1: // Display humidity
+      lcd.setCursor(0, 0);
+      lcd.print("Humid:");
+      lcd.setCursor(6, 0); 
+      lcd.print(humi);
+       lcd.setCursor(0, 1);
+      lcd.print("Mois:");
+      lcd.setCursor(6, 1);
+      lcd.print(soilMois);
+      break;
+  }
 }

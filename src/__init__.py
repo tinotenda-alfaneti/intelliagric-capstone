0f0f9b3c9dde01e32de6e0e19ff3f0b521a1a1e0
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2024-06-28 01:18:15 +0000

    Allow multiple origins

--- src/__init__.py
+++ src/__init__.py
@@ -31,8 +31,7 @@ web_api.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=45)
 Session(web_api)
 
 # ORIGIN_URL = "http://localhost:3000" #TODO: Replace with the deployed version
-ORIGIN_URL = "https://intelli-agric-react-app.vercel.app" #TODO: Replace with the deployed version
-# deployed version = https://intelli-agric-react-m81e69i71.vercel.app/
+ORIGIN_URL = ["https://intelli-agric-react-app.vercel.app", "https://intelli-agric-react-app.vercel.app/"] #TODO: Replace with the deployed version
 
 # secre key
 web_api.secret_key = os.urandom(24)

d4c0243d86776ac57bc152a142d75f919740890d
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2024-07-01 22:55:00 +0000

    Update for disease alerts - frontend fix ripple effect

--- src/controllers/disease_alert_controller.py
+++ src/controllers/disease_alert_controller.py
@@ -1,7 +1,9 @@
+import logging
 from flask_restx import Resource, fields
 from src.config.db_config import database
 from src import api
 from src.controllers.error_controller import handle_errors
+logging.basicConfig(level=logging.DEBUG)
 
 # TODO: add methods interacting with database to firebase module
 ns_broadcast = api.namespace('broadcasts', description='Broadcast operations')
@@ -22,6 +24,7 @@ class BroadcastList(Resource):
         docs = alerts_ref.stream()
         # TODO: Add gpt-3.5 model to get way to deal with the diseases
         alerts = [doc.to_dict() for doc in docs]
+        logging.debug(alerts)
         return alerts, 200
 
 api.add_namespace(ns_broadcast, path='/broadcasts')
